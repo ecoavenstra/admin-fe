2c2d21429c358791d4400b7a40fb622c8e6044f5
Your New Username <axxatagrawal@gmail.com>
2024-08-22 10:47:15 +0530

    build fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-'use client'
-import { useEffect } from 'react';
+'use client';
+import { useEffect, useState } from 'react';
 import AppLayout from "./AppLayout";
 import Dashboard from '../components/Dashboard';
 import { useRouter } from "next/navigation";
@@ -7,27 +7,31 @@ import Loader from '@/components/Loader';
 
 export default function Home() {
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (!token) {
       router.push("/login");
+    } else {
+      setIsLoading(false);
     }
   }, [router]);
 
-//   const handleLogout = () => {
-if(!localStorage.getItem("token")){
-  return(
-    <div className="h-screen flex items-center justify-center">
-      <Loader/>
-    </div>
-  )
-}
+  if (isLoading) {
+    return (
+      <div className="h-screen flex items-center justify-center">
+        <Loader />
+      </div>
+    );
+  }
+
   return (
-    <div className="">
+    <div>
       <AppLayout>
         <Dashboard />
       </AppLayout>
     </div>
+    
   );
 }
